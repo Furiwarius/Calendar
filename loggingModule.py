e921f940208067030b63ecacbc1c9c6ca5aefa0e
Vasiliy <110427738+Furiwarius@users.noreply.github.com>
2023-11-03 10:33:27 +0200

    Update and rename loggingDB.py to loggingModule.py
    Now the logging module is general, and not written for a specific module.

---
+++ loggingModule.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+class Logging():
+    '''Класс логирования модулей календаря
+
+    Отслеживает и записывает данные поступающие в функции для изменения бд,
+    вывода на экран, расчета различных значений'''
+
+    
+    def __init__(self, path:str) -> None :
+        '''Инициализация экземпляра класса
+
+        При создании метод извлекает имя файла и ставит его в название логгера
+        Входящие данные: путь к файлу (str), который нужно обрабатывать.
+        Возвращает: None'''
+        name = self.nameExtraction(path)
+        
+        self.logger = logging.getLogger(name)
+        self.logger.propagate = False
+        
+        # установка уровня логгирования
+        self.logger.setLevel(logging.DEBUG)
+
+        # настройка обработчика
+        self.handler = logging.FileHandler(f"log\{name}_logging.log", mode='w')
+        # настройка форматировщика
+        self.formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+        
+        # добавление форматировщика к обработчику
+        self.handler.setFormatter(self.formatter)
+        # добавление обработчика к логгеру
+        self.logger.addHandler(self.handler)
+
+        self.logger.info(f"Начало работы модуля логгирования {__name__}...")
+
+
+    def nameExtraction(self, path:str) -> str:
+        '''Извлечение имени файла из пути
+
+        Входящие данные: путь к файлу (str), из которого нужно извлечь имя файла.
+        Возвращает: имя файла (str) без пути и без указания расширений'''
+        
+        name = os.path.basename(path)
+        return = name.replace(".py", "")
+        
