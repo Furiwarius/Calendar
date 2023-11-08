d001bb750455693263ec672ea1ba77d5b039e166
Vasiliy <vasiliy.trunov.00@mail.ru>
2023-11-08 21:35:55 +0200

    fix: This module is currently used to test others

---
+++ calendarGUI.py
@@ -0,0 +1,13 @@
+import time
+from consoleMenu import ConsoleMenu, MenuField
+import keyboard
+from colorama import Fore, Back, Style
+
+menu = ConsoleMenu('Календарь')
+
+menu.addField("Поле 1")
+
+
+a = Back.GREEN
+print(type(a))
+
