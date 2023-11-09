9f5c0f5a14ff40e1a944562d099765a0dba21f54
Vasiliy <vasiliy.trunov.00@mail.ru>
2023-11-10 00:34:35 +0200

    feat: The RecordManager module skeleton has been created

---
+++ recordManager.py
@@ -0,0 +1,25 @@
+import datetime
+from calendarDB import CalendarDatabase
+
+class RecordManager():
+
+    def __init__(self) -> None:
+        pass
+
+    def inputDate(self) -> None:
+        pass
+
+    def inputrecord(self) -> None:
+        pass
+
+    def searchDate(self, date:datetime):
+        pass
+
+    def saveRecord(self):
+        pass
+
+    def createRecord(self):
+        pass
+
+    def changeRecord(self):
+        pass
